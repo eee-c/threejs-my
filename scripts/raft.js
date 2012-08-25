4b4214bc26053e5564329329e65c6e07935f9328
Chris Strom <eee.c@eeecooks.com>
2012-08-24 20:44:11 -0400

    Rudder and dirctional motion

--- scripts/raft.js
+++ scripts/raft.js
@@ -42,24 +42,41 @@ function init() {
 
   // scene.add(river_segment);
   // var river1 = riverSegment(scene, 50, 0);
-
-  raft = new Physijs.ConvexMesh(
-    new THREE.TorusGeometry(25, 10, 16, 16),
-    // new THREE.SphereGeometry(20),
-    Physijs.createMaterial(
-      new THREE.MeshNormalMaterial(), 0.2, 0.9
-    )
-  );
-  raft.position.y = 20;
-  raft.rotation.x = Math.PI/2;
-  raft.rotation.z = Math.PI/2;
+  raft = buildRaft();
   scene.add(raft);
+  raft.setDamping(0.0, 1.0);
 
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
   camera.position.y = 500;
   camera.lookAt(raft.position);
 
   scene.add(camera);
+}
+
+function buildRaft() {
+  var raft = new Physijs.ConvexMesh(
+    new THREE.TorusGeometry(25, 10, 16, 16),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    )
+  );
+
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+  var rudder = new THREE.Line(
+    geometry,
+    new THREE.LineBasicMaterial({color: 0xff0000})
+  );
+  rudder.position.z = 0;
+  rudder.position.x = 35;
+  raft.add(rudder);
+
+  raft.position.y = 20;
+  raft.rotation.x = Math.PI/2;
+
+
+  return raft;
 }
 
 function riverSegment(scene, rotation, offset) {
@@ -82,7 +99,7 @@ function riverSegment(scene, rotation, offset) {
   water.rotation.y = rotation;
 
   water.addEventListener('collision', function(object) {
-    raft.applyCentralForce(new THREE.Vector3(1e9 * cos, 0, 1e9 * sin));
+    raft.applyCentralForce(new THREE.Vector3(1e8 * cos, 0, 1e8 * sin));
   });
 
   var bank1 = new Physijs.BoxMesh(
@@ -122,4 +139,33 @@ function render() {
   camera.position.z = raft.position.z;
 
   renderer.render(scene, camera);
+}
+
+document.addEventListener("keydown", function(event) {
+  var code = event.which || event.keyCode;
+  if (code == 0x20) { // space
+    pushRaft();
+  }
+  else if (code == 0x4a) { // J
+    rotateRaft(-1);
+  }
+  else if (code == 0x4b) { // K
+    rotateRaft(1);
+  }
+  else {
+    console.log(code);
+  }
+});
+
+function pushRaft() {
+  var angle = raft.rotation.z
+    , cos = Math.cos(angle)
+    , sin = Math.sin(angle);
+
+  raft.applyCentralForce(new THREE.Vector3(1e8 * cos, 0, 1e8 * sin));
+}
+
+function rotateRaft(direction) {
+  raft.__dirtyRotation = true;
+  raft.rotation.z = raft.rotation.z + direction * Math.PI / 100;
 }
