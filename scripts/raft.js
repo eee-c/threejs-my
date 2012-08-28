86e12241eb692a1823a29a6682e5b76daaef8d69
Chris Strom <eee.c@eeecooks.com>
2012-08-28 19:17:19 -0400

    Group Physijs object for better organization

--- scripts/raft.js
+++ scripts/raft.js
@@ -37,9 +37,20 @@ function init() {
 
   // River
   river_segments = [];
-  offset = riverSegment(scene, Math.PI/8);
-  offset = riverSegment(scene, 0, offset);
-  offset = riverSegment(scene, -Math.PI/8, offset);
+//  buildRiver([R, S, L, L, L, S, L, R, S])
+
+  offset = riverSegment(Math.PI/8);
+  offset = riverSegment(0,          offset);
+  offset = riverSegment(0,          offset);
+  offset = riverSegment(-Math.PI/8, offset);
+  offset = riverSegment(-Math.PI/8, offset);
+  offset = riverSegment(-Math.PI/8, offset);
+  offset = riverSegment(0,          offset);
+  offset = riverSegment(-Math.PI/8, offset);
+  offset = riverSegment(Math.PI/8,  offset);
+  offset = riverSegment(0,          offset);
+
+  riverEnd(offset);
 
   // scene.add(river_segment);
   // var river1 = riverSegment(scene, 50, 0);
@@ -79,49 +90,145 @@ function buildRaft() {
   return raft;
 }
 
-function riverSegment(scene, rotation, offset) {
+function riverSegment(rotation, offset) {
   if (!offset) offset = {x: 0, z: 0};
 
   var length = 1500
-    , sin = Math.sin(rotation)
-    , cos = Math.cos(rotation)
-    , z_frame = 0.5 * length * sin
-    , z_offset = z_frame + offset.z
-    , x_frame = 0.5 * length * cos
-    , x_offset = x_frame + offset.x;
+    , half = length / 2;
+
+  var segment = new Physijs.PlaneMesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.Material()
+  );
+  segment.rotation.y = rotation;
+  segment.position.x = offset.x;
+  segment.position.z = offset.z;
 
   var water = new Physijs.PlaneMesh(
-    new THREE.PlaneGeometry(1500, 500),
+    new THREE.PlaneGeometry(length, 500),
     new THREE.MeshBasicMaterial({color: 0x483D8B})
   );
-  water.position.x = x_offset;
-  water.position.z = -z_offset;
-  water.rotation.y = rotation;
+  water.position.x = half;
+  segment.add(water);
 
-  var bank1 = new Physijs.BoxMesh(
-    new THREE.CubeGeometry(1500, 100, 100),
-    Physijs.createMaterial(
-      new THREE.Material(), 0.2, 0.9
-    ),
-    0
-  );
-  bank1.position.z = -300;
-  water.add(bank1);
+  water.add(bank(-250));
+  water.add(bank(250));
 
-  var bank2 = new Physijs.BoxMesh(
-    new THREE.CubeGeometry(1500, 100, 100),
-    Physijs.createMaterial(
-      new THREE.Material(), 0.2, 0.9
-    ),
-    0
-  );
-  bank2.position.z = 300;
-  water.add(bank2);
+  var end = joint(250, rotation);
+  segment.add(end);
 
-  scene.add(water);
+  scene.add(segment);
   river_segments.push(water);
 
-  return {x: 2 * x_frame + offset.x - 50, z: 2 * z_frame + offset.z};
+  return {
+    x: Math.cos(rotation) * 1500 + offset.x,
+    z: -Math.sin(rotation) * 1500 + offset.z
+  };
+}
+
+function riverEnd(offset) {
+  var wall_width = 100
+    , radius = 750;
+
+  var ref = new Physijs.PlaneMesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.Material()
+  );
+  ref.position.x = offset.x + radius - 100;
+  ref.position.z = offset.z;
+
+  var water = new THREE.Mesh(
+    new THREE.CylinderGeometry(radius, radius, 1),
+    new THREE.MeshBasicMaterial({color: 0x483D8B})
+  );
+  ref.add(water);
+
+  var bottom = new Physijs.BoxMesh(
+    new THREE.CubeGeometry(wall_width, 100, 2 * radius),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    ),
+    0
+  );
+  bottom.position.x = radius;
+  ref.add(bottom);
+
+  var right = new Physijs.BoxMesh(
+    new THREE.CubeGeometry(2 * radius, 100, wall_width),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    ),
+    0
+  );
+  right.position.z = -radius;
+  ref.add(right);
+
+  var left = new Physijs.BoxMesh(
+    new THREE.CubeGeometry(2 * radius, 100, wall_width),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    ),
+    0
+  );
+  left.position.z = radius;
+  ref.add(left);
+
+  scene.add(ref);
+}
+
+
+function bank(offset) {
+  var width = 100
+    , half = width / 2;
+
+  var bank = new Physijs.BoxMesh(
+    new THREE.CubeGeometry(1400, 100, 100),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    ),
+    0
+        );
+  bank.position.x = 100;
+  if (offset < 0) {
+    bank.position.z = offset - half;
+  }
+  else {
+    bank.position.z = offset + half;
+  }
+
+  return bank;
+}
+
+function joint(w, rotation) {
+  var wall_width = 100
+    , wall_length = w + 200
+    , width = w + wall_width / 2;
+
+  var joint = new Physijs.BoxMesh(
+    new THREE.CubeGeometry(wall_length, 100, wall_width),
+    Physijs.createMaterial(
+      new THREE.MeshNormalMaterial(), 0.2, 0.9
+    ),
+    0
+  );
+
+  var water = new THREE.Mesh(
+    new THREE.CylinderGeometry(width, width, 1),
+    new THREE.MeshBasicMaterial({color: 0x483D8B})
+  );
+
+  if (rotation > 0) {
+    joint.position.z = width;
+    water.position.z = -width;
+  }
+  else {
+    joint.position.z = -width;
+    water.position.z = width;
+  }
+
+  joint.add(water);
+
+  return joint;
 }
 
 
